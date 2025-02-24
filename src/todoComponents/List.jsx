250773e3ed935df02efcb86f0d3482e9352616d7
jaehee <jaiyah0417@gmail.com>
2025-02-25 06:50:41 +0900

    리스트 추가

--- src/todoComponents/List.jsx
+++ src/todoComponents/List.jsx
@@ -1,15 +1,37 @@
 import style from '@/Todo.module.css';
 import TodoItem from './TodoItem';
+import { useState } from 'react';
 
 export default function List({ todos }) {
+  const [search, setSearch] = useState('');
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredTodos = () => {
+    if (search === '') {
+      return todos;
+    }
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLowerCase()),
+    );
+  };
+  const filteredTodos = getFilteredTodos();
+
   return (
     <div className={style.list}>
       <h3>Todo List 🌱</h3>
       <div className={style.input_wrapper}>
-        <input type="text" placeholder="검색어를 입력하세요" />
+        <input
+          type="text"
+          placeholder="검색어를 입력하세요"
+          value={search}
+          onChange={onChangeSearch}
+        />
       </div>
       <div>
-        {todos.map((todo) => (
+        {filteredTodos.map((todo) => (
           <TodoItem key={todo.id} {...todo} />
         ))}
       </div>
